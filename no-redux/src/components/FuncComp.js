c0edec7c7423f652b2eeee156381427d61236e79
Drew <drewsup123c@gmail.com>
2019-03-19 17:40:10 -0600

    just messing around with context

--- no-redux/src/components/FuncComp.js
+++ no-redux/src/components/FuncComp.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import {Store} from '../store';
 
+const consoleLog=(message)=>{
+    console.log(message)
+}
+
+const changeMessage = (state) => {
+    state.message = "I changed"
+}
+
 function FuncComp(){
-    const store = React.useContext(Store);
-    console.log(store);
+    // const store = React.useContext(Store);
+    const { state, dispatch } = React.useContext(Store); // comes from Store import above to get the state and dispatch seperatly 
+    console.log(state);
+    const changeNumber = async () => {
+        return dispatch({
+            type:'CHANGE_STATE'
+        })
+    }
+    const send_req = async () => {
+        let res = {};
+        fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => res = response.body)
+        return dispatch({type:'EX_API_REQ', payload:res});
+    }
+    const changeName = async (e,name)=>{
+        return 1;
+    }
     return(
         <div>
             <h1>Functional Component here</h1>
+            <button onClick={() => consoleLog(state.message)} >console log some stuff</button>
+            <button onClick={() => changeMessage(state)}>change message</button>
+            <h1>{state.number}</h1>
+            <button onClick={()=>changeNumber()}>change number</button>
+            <br />
+            <h1>{state.ex_data ? 'Oh yeah it went through' : 'Its false :('}</h1>
+            <button onClick={()=>send_req()}>Send Request</button>
+            <form onSubmit={()=>changeName()}>
+                <input type="text" name="name">name</input>
+                <input type="submit" />
+            </form>
         </div>
     );
 }
--- no-redux/src/store.js
+++ no-redux/src/store.js
@@ -3,15 +3,28 @@ import React from 'react'
 export const Store = React.createContext(); //creates context object
 
 const initialState = {
-    message : "is this better than redux?"
+    message : "is this better than redux?",
+    number : 0,
+    ex_data : false,
 }
 
 function reducer(state, action) {
-    
+    switch(action.type){
+        case 'CHANGE_STATE':
+            return {...state, number:state.number+1 }
+
+        case 'EX_API_REQ':
+            return {...state, ex_data:action.payload}
+
+        default:
+            return state;
+    }
 }
 
 export function StoreProvider(props) {
-    return <Store.Provider value='data from store'>{props.children}</Store.Provider>
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    const value = { state, dispatch }; //creates an object from above statement that holds both the values
+    return <Store.Provider value={value}>{props.children}</Store.Provider>
 } 
-{/* // This will be the react component that will encapsulate the other components in the application. It has an argument of props because that’s how we’ll get access to the other child components. */}
+// This will be the react component that will encapsulate the other components in the application. It has an argument of props because that’s how we’ll get access to the other child components.
 // This will be given to index.js to hold state
